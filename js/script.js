495c63b4905ec341f45a95dfa7c8b09c767872a3
Chojecka Anna, ENT-CDC-CLU-CDY-APC <Anna.Chojecka@swisscom.com>
2017-12-26 01:20:36 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function clickAlert() {
+    alert("Please leave me your name and email address and I will get back to you!");
+    var name = prompt("Name *");
+    var email = prompt("Email *");
+}
